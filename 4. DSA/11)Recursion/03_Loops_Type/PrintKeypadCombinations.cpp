cf3b675beddb0c9c82ca6f738dadc682200b82d9
gokulmaran <64487884+gokulmaran@users.noreply.github.com>
2025-03-19 20:08:06 +0530

    Debug codes added

--- 4. DSA/11)Recursion/03_Loops_Type/PrintKeypadCombinations.cpp
+++ 4. DSA/11)Recursion/03_Loops_Type/PrintKeypadCombinations.cpp
@@ -41,3 +41,61 @@ int main() {
 
     return 0;
 }
+//debug print 
+#include <iostream>
+#include <string>
+using namespace std;
+
+// Array representing the keypad mappings
+string keypad[] = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+
+// Recursive function to generate all possible combinations
+void print1(int num, string output, int depth = 0) { // depth is optional
+    // Indentation for debug output
+    string indent(depth * 2, ' '); 
+    cout << indent << "DEBUG: print(num=" << num << ", output=\"" << output << "\")" << endl;
+
+    if (num == 0) {
+        cout << indent << "DEBUG: Base case reached" << endl;
+        if (output != " ") { // Avoid printing initial empty output
+            cout << indent << "DEBUG: Printing \"" << output << "\"" << endl;
+            cout << output << endl; // Actual output
+        } else {
+            cout << indent << "DEBUG: Skipping empty output" << endl;
+        }
+        return;
+    }
+
+    string temp = keypad[num % 10];
+    cout << indent << "DEBUG: temp = \"" << temp << "\" (keypad[" << (num % 10) << "])" << endl;
+    cout << indent << "DEBUG: num / 10 = " << (num / 10) << endl;
+
+    for (int i = 0; i < temp.size(); i++) {
+        cout << indent << "DEBUG: Calling print with '" << temp[i] << "' + \"" << output << "\"" << endl;
+        print1(num / 10, temp[i] + output, depth + 1); // Pass depth + 1
+    }
+}
+
+// Function to handle the initial call for print combinations
+void printKeypad1(int num) {
+    cout << "DEBUG: printKeypad(" << num << ")" << endl;
+    if (num == 0) {
+        cout << "DEBUG: Edge case num = 0" << endl;
+        cout << " " << endl; // Edge case output
+        return;
+    }
+    string output = "";
+    cout << "DEBUG: Starting recursion with output = \"" << output << "\"" << endl;
+    print1(num, output); // Initial call uses default depth = 0
+}
+
+int main() {
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+
+    cout << "Possible combinations for " << num << ":" << endl;
+    printKeypad1(num);
+
+    return 0;
+}
--- 4. DSA/19) Tries/Problems/LongestCommonPre.cpp
+++ 4. DSA/19) Tries/Problems/LongestCommonPre.cpp
@@ -2,94 +2,116 @@
 #include <vector>
 #include <string>
 using namespace std;
+//link: https://leetcode.com/problems/longest-common-prefix/description/
 
-//Link: https://leetcode.com/problems/longest-common-prefix/
-// TrieNode class
 class TrieNode {
 public:
-    char data;
+    char ch;
     TrieNode* children[26];
-    int childcnt;
     bool isTerminal;
+    int childCnt;
 
-    TrieNode(char data) {
-        this->data = data;
+    TrieNode(char d) {
+        this->ch = d;
         this->isTerminal = false;
-        this->childcnt = 0;
         for (int i = 0; i < 26; i++) {
             this->children[i] = NULL;
         }
+        this->childCnt = 0;
     }
 };
 
-// Trie class
-class Trie {
-public:
-    TrieNode* root;
-
-    Trie() {
-        this->root = new TrieNode('\0');
-    }
-
-    void insertUtil(TrieNode* root, string word) {
-        if (word.size() == 0) {
-            root->isTerminal = true;
-            return;
-        }
-        int ind = word[0] - 'a';
-        if (root->children[ind] != NULL) {
-            insertUtil(root->children[ind], word.substr(1));
-        } else {
-            root->children[ind] = new TrieNode(word[0]);
-            root->childcnt++;
-            insertUtil(root->children[ind], word.substr(1));
-        }
-    }
-
-    void insertWord(string word) {
-        insertUtil(root, word);
-    }
-
-    void lsp(string str, string &ans) {
-        TrieNode* node = root; // Use a local variable to avoid modifying the root
-        for (auto c : str) {
-            if (node->childcnt == 1) {
-                ans.push_back(c);
-                int ind = c - 'a';
-                node = node->children[ind]; // Move to the child node
-            } else {
-                break;
-            }
-            if (node->isTerminal) break;
-        }
-    }
-};
-
-// Solution class
 class Solution {
 public:
-    string longestCommonPrefix(vector<string>& strs) {
-        Trie* t = new Trie();
-        for (auto word : strs) {
-            if (word.empty()) {
-                return "";
-            }
-            t->insertWord(word);
+    void insertWord(TrieNode* root, string word) {
+        cout << "Inserting word: " << word << ", Current node char: " << root->ch << endl;
+
+        // Base case
+        if (word.length() == 0) {
+            root->isTerminal = true;
+            cout << "Reached end of word, set isTerminal = true for char: " << root->ch << endl;
+            return;
         }
+
+        // Process current character
+        char ch = word[0];
+        int index = ch - 'a';
+        TrieNode* child = NULL;
+
+        if (root->children[index] != NULL) {
+            child = root->children[index];
+            cout << "Character '" << ch << "' already exists, moving to child node" << endl;
+        } else {
+            child = new TrieNode(ch);
+            root->childCnt++;
+            root->children[index] = child;
+            cout << "Created new node for '" << ch << "', childCnt incremented to: " << root->childCnt << endl;
+        }
+
+        insertWord(child, word.substr(1));
+    }
+
+    string longestCommonPrefix(vector<string>& strs) {
+        TrieNode* root = new TrieNode('-');
+        cout << "Initialized root node with char: " << root->ch << endl;
+
+        // Insert all strings into the Trie
+        for (int i = 0; i < strs.size(); i++) {
+            cout << "\nInserting string " << i + 1 << ": " << strs[i] << endl;
+            insertWord(root, strs[i]);
+        }
+
+        // Check if root is terminal (empty string case)
+        if (root->isTerminal) {
+            cout << "Root is terminal, returning empty string" << endl;
+            return "";
+        }
+
+        string st = strs[0];
         string ans = "";
-        t->lsp(strs[0], ans);
-        delete t; // Clean up Trie instance
+        cout << "\nFinding longest common prefix using first string: " << st << endl;
+
+        // Traverse Trie to find common prefix
+        for (int i = 0; i < st.length(); i++) {
+            char ch = st[i];
+            int index = ch - 'a';
+            cout << "Checking char '" << ch << "' at position " << i << ", childCnt = " << root->childCnt << endl;
+
+            if (root->childCnt == 1 && root->children[index] != NULL) {
+                ans.push_back(ch);
+                cout << "Added '" << ch << "' to prefix, current prefix: " << ans << endl;
+                root = root->children[index];
+                cout << "Moved to child node with char: " << root->ch << endl;
+
+                if (root->isTerminal) {
+                    cout << "Node is terminal, stopping here" << endl;
+                    break;
+                }
+            } else {
+                cout << "childCnt != 1 or child not found, stopping prefix search" << endl;
+                break;
+            }
+        }
+
+        cout << "Final longest common prefix: " << ans << endl;
         return ans;
     }
 };
 
-// Main function to test the code
+// Main function to test the solution
 int main() {
     Solution solution;
-    vector<string> strs = {"flower", "flow", "flight"};
 
-    string lcp = solution.longestCommonPrefix(strs);
-    cout << "Longest Common Prefix: " << lcp << endl;
+    // Test case
+    vector<string> strs = {"flower", "flow", "flight"};
+    cout << "Input strings:" << endl;
+    for (const string& s : strs) {
+        cout << s << endl;
+    }
+
+    string result = solution.longestCommonPrefix(strs);
+
+    cout << "\nResult: " << result << endl;
 
     return 0;
 }
